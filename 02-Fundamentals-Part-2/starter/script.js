9d601402a5c2cd4e9d04bc49f5e901082988a72d
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-10-22 02:56:35 +0200

    More arrays, plus some Objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -164,20 +164,116 @@
 // console.log(amar);
 // console.log(amar.length);
 
-const calcAge = function (birthyear) {
-  return 2037 - birthyear;
-};
-const years = [1990, 1967, 2002, 2010, 2018];
+// const calcAge = function (birthyear) {
+//   return 2037 - birthyear;
+// };
+// const years = [1990, 1967, 2002, 2010, 2018];
 
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
 
-console.log(age1, age2, age3);
+// console.log(age1, age2, age3);
 
-const ages = [
-  calcAge(years[0]),
-  calcAge(years[1]),
-  calcAge(years[years.length - 1]),
-];
-console.log(ages);
+// const ages = [
+//   calcAge(years[0]),
+//   calcAge(years[1]),
+//   calcAge(years[years.length - 1]),
+// ];
+// console.log(ages);
+
+// //ARRAY METHODS
+// const friends = ["Michael", "Steven", "Peter"];
+// //Add elements
+// const newLength = friends.push("Jay");
+// console.log(friends);
+// console.log(newLength);
+
+// //unshift adds things to the first spot in an array
+// friends.unshift("John");
+// console.log(friends);
+
+// //Remove elements
+// friends.pop();
+// const popped = friends.pop(); // removes last
+// console.log(popped);
+// console.log(friends);
+// friends.shift(); // first
+// console.log(friends);
+// // const shifted = friends.shift();
+// // console.log(shifted);
+
+// console.log(friends.indexOf("Steven")); // shows index
+// console.log(friends.indexOf("Bob"));
+
+// friends.push(23);
+// console.log(friends.includes("Steven")); // returns true or false
+// console.log(friends.includes("Bob"));
+// console.log(friends.includes("23"));
+// console.log(friends.includes(23));
+
+// if (friends.includes("Peter")) {
+//   console.log("You have a friend called Peter");
+// }
+// if (friends.includes("Steven")) {
+//   console.log("You have a friend called Steven");
+// }
+// friends.splice(0, 0); // removes specified item in array, and you can specify how many u want to remove after that one
+// console.log(friends);
+
+//OBJECTS
+// const amarArray = [
+//   "Amar",
+//   "Muric",
+//   2037 - 2004,
+//   "student",
+//   ["Michael", "Steven", "Peter"],
+// ];
+// console.log(amarArray);
+
+// //Name of things insdide --> Properties.
+// const amar = {
+//   firstName: "Amar",
+//   lastName: "Muric",
+//   age: 2037 - 2004,
+//   job: "student",
+//   friends: ["Michael", "Steven", "Peter"],
+// };
+// console.log(amar);
+// //OBJECTS DOT VS BRACKET NOTATION
+
+// console.log(amar.lastName);
+// console.log(amar["lastName"]);
+
+// const nameKey = "Name";
+// console.log(amar["first" + nameKey]);
+// console.log(amar["last" + nameKey]);
+
+// // console.log(jonas."last" + nameKey); <-- cant do this.
+
+// // const interestedIn = prompt(
+// //   "What do you want to know about Amar? Choose between firstName, lastName, age, job and friends"
+// // );
+// // console.log(interestedIn);
+// // console.log(amar[interestedIn]);
+
+// // if (amar[interestedIn]) {
+// //   console.log(amar[interestedIn]);
+// // } else {
+// //   console.log(
+// //     "Wrong request! Choose between firstName, lastName, age, job and friends"
+// //   );
+// // }
+
+// amar.location = "Serbia";
+// amar["github"] = "@AmarMuric04";
+
+// console.log(amar);
+
+// //Challenge
+// //Amar has 3 friends and his best friend is called Michael
+
+// console.log(
+//   `${amar.firstName} has ${amar.friends.length} friends, and his best friend is called ${amar.friends[0]}`
+// );
+// //DONE!
--- Exercises/Fundamentals2.js
+++ Exercises/Fundamentals2.js
@@ -1,3 +1,4 @@
+//Calculating who wins between Dolphins and Koalas
 const calcAverage = (first, second, third) => {
   let average = (first + second + third) / 3;
   return average;
@@ -20,3 +21,25 @@ scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 
 checkWinner(scoreDolphins, scoreKoalas);
+
+/* Write your code below. Good luck! 🙂 */
+
+//Calculating Tip
+let tip = 0
+function calcTip(a) {
+    if (a >= 50 && a <= 300) {
+        tip = a * 0.15;
+        return tip
+    } else {
+        tip = a * 0.20;
+        return tip;
+    }
+}
+const bills = [125,555,44];
+const tips = [calcTip(125),calcTip(555),calcTip(44)];
+const totals = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]];
+
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
