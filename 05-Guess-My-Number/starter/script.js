93558308995b6ed244fb49909db679ec623a16c7
AmarMuric04 <muricamar2004@gmail.com>
2023-10-23 11:57:11 +0200

    Started creating guess the number game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// function getVal() {
+//   const val = document.querySelector('input').value;
+//   return val;
+// }
+// let computerNumber = Math.floor(Math.random() * (20 - 1) + 1);
+// console.log(computerNumber);
+// function guessTheNumber() {
+//   let value = getVal();
+
+//   if (Number(value) == computerNumber) {
+//     document.querySelector('.number').textContent = `${computerNumber}`;
+//   } else {
+//     console.log('loss');
+//   }
+// }
+// document.querySelector('.check').addEventListener('click', guessTheNumber);
+
+console.log(document.querySelector('.message'));
