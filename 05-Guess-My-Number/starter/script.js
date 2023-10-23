d018966d5fee2cb435d33d5cd51aa3c8328a577e
AmarMuric04 <muricamar2004@gmail.com>
2023-10-23 16:29:41 +0200

    Progressing in the Guess the number game.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -26,11 +26,39 @@
 
 // document.querySelector('.guess').value = 2;
 // console.log(document.querySelector('.guess').value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+
   console.log(guess, typeof guess);
   if (!guess) {
     console.log('No number');
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'correct number!';
+    document.querySelector('.number').style.width = '30rem';
+    document.body.style.backgroundColor = '#60b347';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'Too high!';
+    } else {
+      document.querySelector('.message').textContent = 'Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'Too low!';
+      document.querySelector('.score').textContent = 0;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over';
+    }
   }
 });
