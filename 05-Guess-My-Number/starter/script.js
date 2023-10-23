e52aa2e7a5377be10ec9ba0c978867ac55974a93
AmarMuric04 <muricamar2004@gmail.com>
2023-10-23 18:01:02 +0200

    Made code nicer, completely finished the game.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -26,39 +26,89 @@
 
 // document.querySelector('.guess').value = 2;
 // console.log(document.querySelector('.guess').value);
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-document.querySelector('.number').textContent = secretNumber;
+//Made the again button interactive, added a highscore and connected it to localStorage.
+document.querySelector('.highscore').textContent =
+  localStorage.getItem('highscore');
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+// let highscore = 0;
 
-document.querySelector('.check').addEventListener('click', function () {
+document.querySelector('.again').addEventListener('click', playAgain);
+document.querySelector('.check').addEventListener('click', playGame);
+
+function playGame() {
   const guess = Number(document.querySelector('.guess').value);
-
-  console.log(guess, typeof guess);
-  if (!guess) {
-    console.log('No number');
+  if (guess > 20 || guess <= 0) {
+    score -= 2;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent =
+      'Guess a number between 1-20!';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'correct number!';
-    document.querySelector('.number').style.width = '30rem';
-    document.body.style.backgroundColor = '#60b347';
-  } else if (guess > secretNumber) {
-    if (score > 1) {
+    score += 2;
+    updateThings(
+      `${score - 2} + 2`,
+      'correct number!',
+      secretNumber,
+      '30rem',
+      '#60b347'
+    );
+
+    highScore();
+    // if (score > highscore) {
+    //   highscore = score;
+    //   document.querySelector('.highscore').textContent = highscore;
+    // }
+  } else if (guess !== secretNumber) {
+    if (score >= 1) {
+      updateContent(
+        `${score}`,
+        guess > secretNumber ? 'Too high!' : 'Too low',
+        '?',
+        '15rem',
+        '#222'
+      );
       score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = 'Too high!';
     } else {
-      document.querySelector('.message').textContent = 'Game Over';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = 'Too low!';
-      document.querySelector('.score').textContent = 0;
-    } else {
-      document.querySelector('.message').textContent = 'Game Over';
+      updateContent(0, 'Game Over', '?', '15rem', '#222');
+      secretNumber = Math.random();
     }
   }
-});
+}
+function playAgain() {
+  score = 20;
+  updateContent(score, 'Start guessing...', '?', '15rem', '#222');
+  document.querySelector('.guess').value = '';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+}
+/*We take the score that shows when the user correctly guesses a number,
+then we push it into an array, we will take the first score ever as the highest
+current score, using that score we can get every other new highscore using
+if currenthighscore < currentscore, then the currentscore becomes the new highscore.*/
+let highscore = [];
+function highScore() {
+  highscore.push(score);
+  for (let i = 0; i < highscore.length; i++) {
+    let high = highscore[0];
+    if (high < highscore[i]) {
+      high = highscore[i];
+    }
+    document.querySelector('.highscore').textContent = high;
+    localStorage.setItem('highscore', high);
+  }
+}
+const updateContent = function (
+  scoreText,
+  messageText,
+  numberText,
+  numberWidth,
+  bodyColor
+) {
+  document.querySelector('.score').textContent = scoreText;
+  document.querySelector('.message').textContent = messageText;
+  document.querySelector('.number').textContent = numberText;
+  document.querySelector('.number').style.width = numberWidth;
+  document.body.style.backgroundColor = bodyColor;
+};
---
+++ Exercises/GuesstheNumber/app.js
@@ -0,0 +1,11 @@
+// document.querySelector(".again").addEventListener("click", playAgain);
+
+// function playAgain() {
+//   score = 20;
+//   document.querySelector(".guess").value = "";
+//   document.querySelector(".number").style.width = "15rem";
+//   document.querySelector(".score").textContent = score;
+//   document.querySelector(".number").textContent = "?";
+//   document.body.style.backgroundColor = "#222";
+//   secretNumber = Math.trunc(Math.random() * 20) + 1;
+// }
