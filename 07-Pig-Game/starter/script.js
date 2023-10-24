3ed26163aa4cd296730da3a9a1ec3b65af9d2066
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-10-24 17:18:24 +0200

    The pig game is completely done

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -33,20 +33,19 @@
 // function holdButton() {
 //   scorePlayerOne.textContent += currScorePlayerOne
 // }
+let scores = [0, 0];
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+let current0El = document.getElementById('current--0');
+let current1El = document.getElementById('current--1');
 const btnRoll = document.querySelector('.btn--roll');
-let diceImage = document.querySelector('.dice');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
-let score0El = document.querySelector('#score--0');
-let score1El = document.getElementById('score--1');
 const btnHold = document.querySelector('.btn--hold');
-
-const scores = [0, 0];
-let currentScore = 0;
+const btnNew = document.querySelector('.btn--new');
+let score0El = document.getElementById('score--0');
+let score1El = document.getElementById('score--1');
+let diceImage = document.querySelector('.dice');
 let activePlayer = 0;
-
+let currentScore = 0;
 score0El.textContent = 0;
 score1El.textContent = 0;
 
@@ -57,16 +56,49 @@ btnRoll.addEventListener('click', function () {
 
   diceImage.classList.remove('hidden');
   diceImage.src = `dice-${dice}.png`;
-  console.log(dice);
   if (dice !== 1) {
     currentScore += dice;
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
   }
+});
+btnHold.addEventListener('click', function () {
+  document.getElementById(`score--${activePlayer}`).textContent = scores[
+    activePlayer
+  ] += currentScore;
+  if (scores[0] >= 100 || scores[1] >= 100) {
+    document.getElementById(`score--${activePlayer}`).textContent = 'You win';
+    btnHold.classList.add('hidden');
+    btnRoll.classList.add('hidden');
+    diceImage.classList.add('hidden');
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+  } else {
+    if (currentScore !== 0) {
+      switchPlayer();
+    } else {
+      alert('You must roll at least once');
+    }
+  }
+});
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+
+btnNew.addEventListener('click', function () {
+  activePlayer = 0;
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  btnHold.classList.remove('hidden');
+  btnRoll.classList.remove('hidden');
+  diceImage.classList.add('hidden');
+  scores = [0, 0];
+  currentScore = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
 });
