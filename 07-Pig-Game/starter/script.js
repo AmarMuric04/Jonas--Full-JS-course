6edb5459674e140af7dff0368bd021c7586bb028
AmarMuric04 <muricamar2004@gmail.com>
2023-10-24 12:41:15 +0200

    Almost done with The Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,10 +1,4 @@
-// 'use strict';
-
-// const btnRoll = document.querySelector('.btn--roll');
-// let diceImage = document.querySelector('.dice');
-// let currScorePlayerOne = document.getElementById('current--0');
-// let scorePlayerOne = document.getElementById('score--0');
-// let hold = document.querySelector('.btn--hold');
+'use strict';
 
 // hold.addEventListener('click', holdButton);
 // btnRoll.addEventListener('click', rollDice);
@@ -37,5 +31,38 @@
 // }
 
 // function holdButton() {
-//   scorePlayerOne.textContent += currScorePlayerOne.textContent;
+//   scorePlayerOne.textContent += currScorePlayerOne
 // }
+const btnRoll = document.querySelector('.btn--roll');
+let diceImage = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+let score0El = document.querySelector('#score--0');
+let score1El = document.getElementById('score--1');
+const btnHold = document.querySelector('.btn--hold');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+diceImage.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceImage.classList.remove('hidden');
+  diceImage.src = `dice-${dice}.png`;
+  console.log(dice);
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
