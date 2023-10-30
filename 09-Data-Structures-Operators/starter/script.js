7a170fe5c27c09cbdcc7fcd2019fb4891fcfa05a
AmarMuric04 <muricamar2004@gmail.com>
2023-10-30 12:46:31 +0100

    Spread function, similar to concat but better?

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -25,6 +25,9 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} ${time}`
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`here is ur delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
 
   openingHours: {
     thu: {
@@ -47,40 +50,40 @@ const restaurant = {
 //   mainIndex: 2,
 //   starterIndex: 2,
 // });
-restaurant.orderDelivery({
-  address: 'Via del Sole, 21',
-  starterIndex: 2,
-});
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+// restaurant.orderDelivery({
+//   address: 'Via del Sole, 21',
+//   starterIndex: 2,
+// });
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
 
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurant, hours, tags);
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurant, hours, tags);
 
-//Default values
-const { menu = [], starterMenu: starters = [] } = restaurant;
+// //Default values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
 
-console.log(menu, restaurant);
+// console.log(menu, restaurant);
 
-//Mutating variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
+// //Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
 
-({ a, b } = obj);
-console.log(a, b);
+// ({ a, b } = obj);
+// console.log(a, b);
 
-//Nested objects
-const {
-  fri: { open: o, close: c },
-} = openingHours;
-console.log(o, c);
-// ({ open, close } = fri);
-// console.log(open, close);
+// //Nested objects
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+// // ({ open, close } = fri);
+// // console.log(open, close);
 
 // const array = [1, 2, 3];
 
@@ -128,4 +131,57 @@ console.log(o, c);
 // const [o = 1, p = 1, r = 1, s = 1, t = 1] = [5, 4, 3, 2];
 // console.log(o, p, r, s, t);
 
-// //DESTRUCTURING OBJECTS
+// //DESTRUCTURING OBJECTS UP ABOVE
+
+const arr = [7, 8, 9];
+// const arr2 = [1, 2, 3];
+const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArray);
+
+//SPREAD OPERATOR
+const newArray = [1, 2, ...arr];
+console.log(newArray);
+console.log(...newArray);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci', 'Birijan', 'Tursija'];
+console.log(newMenu);
+
+// let arr3 = arr.concat(arr2);
+// console.log(arr3);
+// arr3 = [...arr, ...arr2];
+// console.log(arr3);
+
+//copy array
+
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//join 2 arrays
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+//Iterables are arrays, strings, maps and sets, but not OBJECTS.
+const str = 'Amar';
+const letters = [...str];
+console.log(letters);
+console.log(...str);
+// const openingHoursCopy = { ...restaurant.openingHours };
+// console.log(openingHoursCopy);
+
+//real world example
+// const ingrediants = [
+//   prompt('First ingrediant'),
+//   prompt('Second ingrediant'),
+//   prompt('Ingrediant 3'),
+// ];
+
+// console.log(ingrediants);
+// restaurant.orderPasta(...ingrediants);
+
+//objects
+const restaurantCopy = { foundedIn: 2004, ...restaurant, status: 'Closed' };
+console.log(restaurantCopy);
+
+const objectCopy = { ...restaurant };
+objectCopy.name = 'Murgin kafic';
+console.log(objectCopy);
+console.log(restaurant);
