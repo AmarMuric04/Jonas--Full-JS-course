deca5e6c9eedb279997b84bc4861abea365c3f65
AmarMuric04 <muricamar2004@gmail.com>
2023-10-31 12:27:38 +0100

    Started doing Coding Challenge #2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,6 +5,24 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const [one, two, three, four, five, ...otherDays] = weekdays;
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -28,6 +46,8 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`here is ur delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
   },
+
+  //ES6 FUNCTION
   orderPizza(mainIngrediant, ...otherIngrediats) {
     if (otherIngrediats.length === 0) {
       otherIngrediats = ' only';
@@ -36,21 +56,63 @@ const restaurant = {
       `Your pizza with${otherIngrediats} the main ingrediant ${mainIngrediant} has been delivered!`
     );
   },
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+
+  //ES6 OBJECT LITERAL
+
+  openingHours,
 };
+console.log(restaurant);
+restaurant && console.log(restaurant);
+
+//WITH OPTIONAL CHAINING
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant?.openingHours?.four?.open);
+
+//Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// for (let i = 0; i < days.length; i++) {
+//   let day = days[i];
+//   console.log(restaurant?.openingHours?.days[i].open);
+// }
+for (const day of days) {
+  console.log(
+    `on ${day}, we open at ${restaurant.openingHours[day]?.open ?? 'closed'}`
+  );
+}
+
+//Methods
+
+restaurant?.orderPizza('Olives', 'Tomatoes', 'Potatoes', 'a salad');
+console.log(restaurant?.order(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.() ?? 'Method does not exist');
+
+//ARRAYS
+const users = [{ name: 'Amar', email: 'muricamar2004@gmail.com' }];
+console.log(users?.[0] ?? 'Specified user does not exist');
+
+// ?? ??= && &&= ||= ?. ...
+
+const properties = Object.keys(openingHours);
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of Object.keys(openingHours)) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+//PROPERTY VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entries for object
+const entries = Object.entries(openingHours);
+
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key}, we open at ${open} and close at ${close}`);
+}
 // restaurant.orderDelivery({
 //   time: '23:30',
 //   address: 'Via del Sole, 21',
@@ -352,3 +414,5 @@ const restaurant = {
 // rest1.owner &&= '<ANONYMOUS>';
 // console.log(rest2);
 // console.log(rest1);
+
+//ENHANCED OBJECT LITERALS
