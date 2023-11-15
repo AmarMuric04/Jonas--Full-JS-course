7c789234c841a4b3a60dad8ae039181043bd4177
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-11-15 04:24:00 +0100

    More work with numbers, more methods...

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -126,13 +126,13 @@ const calcSummary = function (movement) {
   const totalIn = movement.movements
     .filter(e => e > 0)
     .reduce((acc, e) => acc + e, 0);
-  labelSumIn.textContent = `${(totalIn + interest).toFixed(1)}€`;
+  labelSumIn.textContent = `${(totalIn + interest).toFixed(2)}€`;
 
   /*CALCULATING HOW MUCH WENT OUT THE ACCOUNT*/
   const totalOut = movement.movements
     .filter(e => e < 0)
     .reduce((acc, e) => acc + e, 0);
-  labelSumOut.textContent = `${Math.abs(totalOut)}€`;
+  labelSumOut.textContent = `${Math.abs(totalOut).toFixed(2)}€`;
 };
 
 // console.log(account1.interestRate);
@@ -232,20 +232,21 @@ btnTransfer.addEventListener('click', function (e) {
 /*REQUEST A LOAN*/
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const request = +inputLoanAmount.value;
+  /*OR Math.trunc() because we are working exclusively with positive numbers */
+  const request = Math.floor(inputLoanAmount.value);
   /*CHECK IF THE USER HAS A DEPOSIT THATS AT LEAST 10% OF THE REQUESTED LOAN */
   const requestRequirement = currentAccount.movements.some(
     deposit => deposit >= request / 10
   );
-  /*IF THE USER HAS A DEPOSIT THATS AT LEAST 10% OF THE REQUEST*/
-  if (requestRequirement) {
+  /*IF THE USER HAS A DEPOSIT THATS AT LEAST 10% OF THE REQUEST (+)*/
+  if (requestRequirement && request > 0) {
     currentAccount.movements.push(request);
     inputLoanAmount.value = '';
     inputLoginPin.blur();
     setTimeout(() => {
       updateUI();
     }, /*1000 DOLLARS IS EQUAL TO 1 SECOND OF WAITING TIME*/ 1000 * (request / 1000));
-  } /*IF HE DOESNT*/ else console.log('Error');
+  } /*IF HE DOESNT (+)*/ else console.log('Error');
 });
 
 /*CLOSING ACCOUNT */
@@ -327,34 +328,117 @@ btnChangeTheme.addEventListener('click', function () {
 /////////////////////////////////////////////////
 // LECTURES
 
-console.log(23 === 23.0);
+// console.log(23 === 23.0);
 
-//base 10 -> 0 to 9
-//binary base -> 0 and 1
-console.log(0.2 + 0.1);
+// //base 10 -> 0 to 9
+// //binary base -> 0 and 1
+// console.log(0.2 + 0.1);
 
-console.log(0.1 + 0.2 === 0.3);
+// console.log(0.1 + 0.2 === 0.3);
 
-//Converting strings to numbers
-console.log(Number('23'));
-console.log(+'23');
+// //Converting strings to numbers
+// console.log(Number('23'));
+// console.log(+'23');
 
-//parsing
-console.log(Number.parseInt('30px', 10));
-console.log(Number.parseInt('e30px', 10));
+// //parsing
+// console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseInt('e30px', 10));
 
-console.log(Number.parseFloat('   2.5rem  ', 10));
-console.log(Number.parseInt('  2.5rem  ', 10));
+// console.log(Number.parseFloat('   2.5rem  ', 10));
+// console.log(Number.parseInt('  2.5rem  ', 10));
 
-console.log(parseInt('25vh'));
+// console.log(parseInt('25vh'));
 
-//Check if a value is a number
-console.log(Number.isNaN(+'a3'));
-console.log(Number.isNaN(23 / 0));
-//returns infinity
+// //Check if a value is a number (value is not a number)
+// console.log(Number.isNaN(+'a3'));
+// console.log(Number.isNaN(23 / 0));
+// //returns infinity
 
-//best way to check if a value is a number
-console.log(Number.isFinite(20));
-console.log(Number.isFinite(20 / 0));
-console.log(Number.isFinite('e0'));
-console.log(Number.isFinite(+'20'));
+// //best way to check if a value is a number
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite(20 / 0));
+// console.log(Number.isFinite('e0'));
+// console.log(Number.isFinite(+'20'));
+
+// console.log(Number.isInteger(20));
+// console.log(Number.isInteger(20.5));
+// console.log(Number.isInteger(20 / 6));
+// console.log(Number.isInteger(20 / 5));
+
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+
+// const array = [5, 2, 3, 4, 5, 2, 7];
+// console.log(Math.max(...array));
+// console.log(Math.max(5, 2, '23', 5));
+// console.log(Math.max(5, 2, '23px', 5));
+
+// console.log(Math.min(5, 2, '23', 5));
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// console.log(Math.trunc(Math.random() * 6) + 1);
+
+// const randomizer = (min, max) =>
+//   Math.floor(Math.random() * (max - min) + 1) + min;
+// //0....1
+
+// console.log(randomizer(10, 20));
+
+// //rounding integers
+// console.log(Math.trunc(23.9));
+// console.log(Math.round(23.9));
+
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(23.9));
+// //THEY ALL DO TYPE COARSION "20" => 20
+
+// //floor and trunc do the same thing when dealing with positive numbers
+// console.log(Math.trunc(-23.3)); //-23
+// console.log(Math.floor(-23.3)); //-24
+
+// //rounding decimals
+
+// console.log((2.7).toFixed(0));
+// console.log((2.7).toFixed(3));
+// console.log((2.345).toFixed(2));
+// console.log(+(2.345).toFixed(3));
+
+// console.log(5 % 2);
+// console.log(5 / 2); // 5 = 2 + 2 + (1)
+// console.log(8 % 3);
+// console.log(8 / 3); // 8 = 2 + 2 + 2 + (2)
+
+// console.log(4 % 2 === 0);
+
+// document.body.addEventListener('keydown', function (event) {
+//   if (event.key === 'Enter') {
+//     [...document.querySelectorAll('.movements__row')].forEach(function (
+//       row,
+//       i
+//     ) {
+//       i % 2 === 0
+//         ? (row.style.backgroundColor = 'orangered') //0,2,4,6,8
+//         : (row.style.backgroundColor = 'blue'); //0,1,3,5,7
+//     });
+//   }
+// });
+
+//287,460,000,000
+const diameter = 287_460_000_000;
+
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1_415;
+console.log(PI);
+
+console.log(Number('230000'));
