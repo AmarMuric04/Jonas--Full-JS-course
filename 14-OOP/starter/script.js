2fd639a7cc76894c0ad0c30f8c46a2135345f1c3
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-11-28 20:13:15 +0100

    prototype inheritance, looking at preex. objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -12,7 +12,6 @@ const Person = function (firstName, birthYear) {
   //     console.log(currYear - birthYear);
   //   };
 };
-``;
 
 const amar = new Person('Amar', 2004);
 // amar.calcAge()
@@ -46,3 +45,38 @@ matilda.calcAge();
 jack.calcAge();
 
 console.log(amar.__proto__);
+console.log(amar.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(amar));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+
+console.log(amar.species, matilda.species);
+console.log(amar.hasOwnProperty('firstName'));
+console.log(amar.hasOwnProperty('species'));
+
+console.log(Person.prototype.constructor);
+
+console.log(amar.__proto__);
+//object.prototype
+console.log(amar.__proto__.__proto__);
+// null
+console.log(amar.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 2, 3, 4, 6, 9, 0]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.log(h1.__proto__);
+
+console.dir(x => x + 1);
