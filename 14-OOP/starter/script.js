aa5fefbe1eb1a6f5ab870b21024b4934d31e3186
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-11-29 19:15:07 +0100

    getters and setters with oop

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,82 +1,156 @@
 'use strict';
 
-const currYear = new Date().getFullYear();
+// const currYear = new Date().getFullYear();
 
-const Person = function (firstName, birthYear) {
-  //instane properties
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   //instane properties
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  //never use this!
-  //   this.calcAge = function () {
-  //     console.log(currYear - birthYear);
-  //   };
-};
+//   //never use this!
+//   //   this.calcAge = function () {
+//   //     console.log(currYear - birthYear);
+//   //   };
+// };
 
-const amar = new Person('Amar', 2004);
-// amar.calcAge()
-// 1. new empty object is created
-// 2. function is called(this keyword is this new empty object)
-// 3. {} linked to prototype
-// 4. function automatially returns {}
-// console.log(typeof Person);
+// const amar = new Person('Amar', 2004);
+// // amar.calcAge()
+// // 1. new empty object is created
+// // 2. function is called(this keyword is this new empty object)
+// // 3. {} linked to prototype
+// // 4. function automatially returns {}
+// // console.log(typeof Person);
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1994);
-console.log(amar, matilda, jack);
+// const matilda = new Person('Matilda', 2017);
+// const jack = new Person('Jack', 1994);
+// console.log(amar, matilda, jack);
+// // amar.calcAge();
+// // matilda.calcAge();
+// // jack.calcAge();
+
+// // const jay = 'jay';
+
+// // console.log(amar instanceof Person);
+// // console.log(jay instanceof Person);
+
+// //prototypes
+// console.log(Person.prototype);
+
+// Person.prototype.calcAge = function () {
+//   console.log(currYear - this.birthYear);
+// };
+
 // amar.calcAge();
 // matilda.calcAge();
 // jack.calcAge();
 
-// const jay = 'jay';
+// console.log(amar.__proto__);
+// console.log(amar.__proto__ === Person.prototype);
 
-// console.log(amar instanceof Person);
-// console.log(jay instanceof Person);
+// console.log(Person.prototype.isPrototypeOf(amar));
+// console.log(Person.prototype.isPrototypeOf(Person));
 
-//prototypes
-console.log(Person.prototype);
+// Person.prototype.species = 'Homo Sapiens';
 
-Person.prototype.calcAge = function () {
-  console.log(currYear - this.birthYear);
+// console.log(amar.species, matilda.species);
+// console.log(amar.hasOwnProperty('firstName'));
+// console.log(amar.hasOwnProperty('species'));
+
+// console.log(Person.prototype.constructor);
+
+// console.log(amar.__proto__);
+// //object.prototype
+// console.log(amar.__proto__.__proto__);
+// // null
+// console.log(amar.__proto__.__proto__.__proto__);
+
+// console.dir(Person.prototype.constructor);
+
+// const arr = [3, 6, 4, 2, 3, 4, 6, 9, 0]; // new Array === []
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+// console.log(arr.__proto__.__proto__);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.log(h1.__proto__);
+
+// console.dir(x => x + 1);
+
+//class expression
+// const PersonCl = class {};
+
+//class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  //methos will be added to .prototype propert y
+  calcAge() {
+    console.log(2023 - this.birthYear);
+  }
+  greet() {
+    console.log(`hey ` + this.fullName);
+  }
+
+  get age() {
+    return 2023 - this.birthYear;
+  }
+
+  //set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else alert(`${name} is not a full name`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Amar Muric', 2005);
+console.log(jessica.age, 123);
+
+console.log(jessica);
+console.log(jessica.fullName);
+
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`hey ` + this.firstName);
+// };
+
+jessica.greet();
+
+// 1.Classes are not hoisted
+// 2.Classes are first class citizens
+// 3.Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 2005);
+
+console.log(walter.fullName);
+
+//getter and setter properties
+const account = {
+  owner: 'amar',
+  movements: [1, 2, 3, 4, 5, 6, 7, 2, 98, 213, 4123, 124],
+
+  get latest() {
+    return this.movements[this.movements.length - 1];
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
 };
-
-amar.calcAge();
-matilda.calcAge();
-jack.calcAge();
-
-console.log(amar.__proto__);
-console.log(amar.__proto__ === Person.prototype);
-
-console.log(Person.prototype.isPrototypeOf(amar));
-console.log(Person.prototype.isPrototypeOf(Person));
-
-Person.prototype.species = 'Homo Sapiens';
-
-console.log(amar.species, matilda.species);
-console.log(amar.hasOwnProperty('firstName'));
-console.log(amar.hasOwnProperty('species'));
-
-console.log(Person.prototype.constructor);
-
-console.log(amar.__proto__);
-//object.prototype
-console.log(amar.__proto__.__proto__);
-// null
-console.log(amar.__proto__.__proto__.__proto__);
-
-console.dir(Person.prototype.constructor);
-
-const arr = [3, 6, 4, 2, 3, 4, 6, 9, 0]; // new Array === []
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-console.log(arr.__proto__.__proto__);
-
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
-console.log(arr.unique());
-
-const h1 = document.querySelector('h1');
-console.log(h1.__proto__);
-
-console.dir(x => x + 1);
+console.log(account.latest);
+console.log((account.latest = 123));
+console.log(account.movements);
---
+++ Exercises/OOP/app..js
@@ -0,0 +1,53 @@
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+console.log("123");
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+  console.log(this.make + " Starting speed: " + this.speed + "km/h");
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.make + " After accelerating: " + this.speed + "km/h");
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.make + " After braking: " + this.speed + "km/h");
+};
+
+const BMW = new Car("BMW", 120);
+const Mercedes = new Car("Mercedes", 95);
+
+BMW.accelerate();
+BMW.accelerate();
+BMW.accelerate();
+Mercedes.brake();
+BMW.brake();
+Mercedes.brake();
+Mercedes.accelerate();
+Mercedes.brake();
+BMW.brake();
+Mercedes.accelerate();
+BMW.accelerate();
+Mercedes.brake();
+BMW.accelerate();
+BMW.brake();
+Mercedes.accelerate();
+Mercedes.brake();
+Mercedes.accelerate();
+BMW.brake();
+BMW.brake();
+Mercedes.brake();
