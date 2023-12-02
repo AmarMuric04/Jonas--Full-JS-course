8a67c10979b40dadd93cc2786a58374df817b462
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-12-02 17:55:18 +0100

    Markers and form functioning

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude } = position.coords;
@@ -19,36 +21,68 @@ navigator.geolocation.getCurrentPosition(
 
     const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 13);
-
-    console.log(map);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    map.on('click', function (mapEvent) {
-      L.marker(mapEvent.latlng)
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent(
-          `Workour ${months[new Date().getMonth()]} ${String(
-            new Date().getDate()
-          ).padStart(2, 0)}`
-        )
-        .openPopup();
-    });
+    //Handling clicks on map
+    map.on('click', showMarker);
   },
+
   function () {
     alert('Could not get your position.');
   }
 );
+function showMarker(mapE) {
+  mapEvent = mapE;
+  form.classList.remove('hidden');
+  inputDistance.focus();
+  console.log(1);
+  map.removeEventListener('click', showMarker);
+}
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCadence.value === '' ||
+    inputDistance.value === '' ||
+    inputDuration.value === ''
+  )
+    return;
+  //clear input fields
+  inputCadence.value =
+    inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+
+  console.log(mapEvent);
+  //Display marker
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent(
+      `Workout ${months[new Date().getMonth()]} ${String(
+        new Date().getDate()
+      ).padStart(2, 0)}`
+    )
+    .openPopup();
+  map.on('click', showMarker);
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('div').classList.toggle('form__row--hidden');
+  inputCadence.closest('div').classList.toggle('form__row--hidden');
+});
