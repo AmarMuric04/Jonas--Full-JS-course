a514c0965d5f39245159837d414a0f33123d30db
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-12-15 20:19:27 +0100

    finished coding challenge #1

--- Exercises/async/app.js
+++ Exercises/async/app.js
@@ -1,5 +1,41 @@
 // Coding Challenge #1
 
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+const renderCountry = function (data, className = "") {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const getJSON = function (url, errorMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
 /* 
 In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
 
@@ -23,10 +59,59 @@ TEST COORDINATES 2: -33.933, 18.474
 
 GOOD LUCK 😀
 */
-const whereAmI = function (lat, lng) {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+btn.addEventListener("click", function () {
+  // whereAmI(52.508, 13.381);
+  // whereAmI(19.037, 72.873);
+  whereAmI(-33.933, 18.474);
+});
+const getCountryData = function (country) {
+  // Country 1
+  getJSON(
+    `https://countries-api-836d.onrender.com/countries/name/${country}`,
+    "Country not found"
+  )
+    .then((data) => {
+      renderCountry(data[0]);
+      // const neighbour = data[0].borders[0];
+
+      // if (!neighbour) throw new Error("No neighbour found!");
+
+      // Country 2
+      // return getJSON(
+      // `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
+      // "Country not found"
+      // );
+    })
+
+    // .then((data) => renderCountry(data, "neighbour"))
+    .catch((err) => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
-whereAmI(52.508, 13.381);
-console.log(123);
+
+var requestOptions = {
+  method: "GET",
+};
+const whereAmI = function (lat, lng) {
+  fetch(
+    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&apiKey=5732bd3686e246b1803525c7c575d132`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let mainData = data.features[0].properties;
+      getCountryData(mainData.country);
+    })
+    .catch((err) => {
+      console.error("Lost internet connection");
+      console.log(`Error ${err}`);
+    });
+};
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
