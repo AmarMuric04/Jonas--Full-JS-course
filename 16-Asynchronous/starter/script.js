1858183350ffd0681ccaf966124df415f94d804c
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-12-16 17:46:05 +0100

    Promisifying geolocation

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -186,18 +186,18 @@ const getCountryData = function (country) {
   )
     .then(data => {
       renderCountry(data[0]);
-      const neighbour = data[0].borders[0];
+      // const neighbour = data[0].borders[0];
 
-      if (!neighbour) throw new Error('No neighbour found!');
+      // if (!neighbour) throw new Error('No neighbour found!');
 
-      // Country 2
-      return getJSON(
-        `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
-        'Country not found'
-      );
+      // // Country 2
+      // return getJSON(
+      //   `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
+      //   'Country not found'
+      // );
     })
 
-    .then(data => renderCountry(data, 'neighbour'))
+    // .then(data => renderCountry(data, 'neighbour'))
     .catch(err => {
       console.error(`${err} 💥💥💥`);
       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
@@ -208,7 +208,7 @@ const getCountryData = function (country) {
 };
 
 btn.addEventListener('click', function () {
-  getCountryData('portugal');
+  whereAmI();
 });
 
 // getCountryData('australia');
@@ -227,8 +227,85 @@ btn.addEventListener('click', function () {
 // console.log('Test end');
 //creating promises
 
-const lotteryPromise = new Promise(function (resolve, reject) {
-  if (Math.random() >= 0.5) {
-    resolve();
-  } else reject();
-});
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Ticket bought.');
+//   setTimeout(() => {
+//     if (Math.random() >= 0.5) resolve('You win');
+//     else reject(new Error('You lost your money'));
+//   }, 2000);
+// });
+
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// promisifying settimeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 2000);
+//   });
+// };
+
+// wait(2)
+//   .then(() => {
+//     console.log('Waited 2 seconds');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('Waited 3 seconds');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('Waited 4 seconds');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('Waited 5 seconds');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('Waited 6 seconds');
+//     return wait(1);
+//   })
+//   .then(() => console.log('Waited 1 seconds'));
+
+// Promise.resolve('abc').then(res => console.log(res));
+// Promise.reject(new Error('Problem!')).catch(err => console.error(err));
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition()
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+var requestOptions = {
+  method: 'GET',
+};
+const whereAmI = function (lat, lng) {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+      return fetch(
+        `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&apiKey=5732bd3686e246b1803525c7c575d132`,
+        requestOptions
+      );
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      let mainData = data.features[0].properties;
+      console.log(`You are in ${mainData.city}, ${mainData.country}`);
+
+      getCountryData(mainData.country);
+    })
+    .catch(err => {
+      console.error('Lost internet connection');
+      console.log(`Error ${err}`);
+    });
+};
