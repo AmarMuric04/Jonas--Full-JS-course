bc8b2e51f9717f614c61d05dbaa0c5c6bd089af8
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-12-16 15:48:53 +0100

    Manually creating promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -212,3 +212,23 @@ btn.addEventListener('click', function () {
 });
 
 // getCountryData('australia');
+// console.log('Test start');
+
+// setTimeout(() => {
+//   console.log('0sec timeout');
+// }, 0);
+// Promise.resolve('Resolved promise 1#').then(res => console.log(res));
+
+// Promise.resolve('Resolved promise 2#').then(res => {
+//   for (let i = 0; i < 1000000000; i++) {}
+//   console.log(res);
+// });
+
+// console.log('Test end');
+//creating promises
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  if (Math.random() >= 0.5) {
+    resolve();
+  } else reject();
+});
