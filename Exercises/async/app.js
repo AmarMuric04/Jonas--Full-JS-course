5f775063e8b31c4d8ba2d2aa7835807aad1029e5
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-12-23 23:23:40 +0100

    JS

--- Exercises/async/app.js
+++ Exercises/async/app.js
@@ -146,6 +146,8 @@ const wait = function (seconds) {
 
 const imgContainer = document.querySelector(".images");
 
+let currImg;
+
 const createImage = function (imgPath) {
   return new Promise(function (resolve, reject) {
     const img = document.createElement("img");
@@ -155,112 +157,30 @@ const createImage = function (imgPath) {
       imgContainer.append(img);
       resolve(img);
     });
-
     img.addEventListener("error", function () {
       reject(new Error("Image not found"));
     });
   });
 };
 
-let currentImg;
-
-createImage("img/img-1.jpg")
+createImage("/16-Asynchronous/final/img/img-1.jpg")
   .then((img) => {
-    currentImg = img;
-    console.log("Image 1 loaded");
+    currImg = img;
+    console.log("image 1 loaded");
     return wait(2);
   })
   .then(() => {
-    currentImg.style.display = "none";
-    return createImage("img/img-2.jpg");
+    currImg.style.display = "none";
+    return createImage("/16-Asynchronous/final/img/img-2.jpg");
   })
   .then((img) => {
-    currentImg = img;
-    console.log("Image 2 loaded");
+    currImg = img;
+    console.log("image 2 loaded");
     return wait(2);
   })
   .then(() => {
-    currentImg.style.display = "none";
+    currImg.style.display = "none";
   })
-  .catch((err) => console.error(err));
-
-///////////////////////////////////////
-// Consuming Promises with Async/Await
-// Error Handling With try...catch
-
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
+  .catch((err) => {
+    console.error(err);
   });
-};
-
-// fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(res => console.log(res))
-
-const whereAmI = async function () {
-  try {
-    // Geolocation
-    const pos = await getPosition();
-    const { latitude: lat, longitude: lng } = pos.coords;
-
-    // Reverse geocoding
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    if (!resGeo.ok) throw new Error("Problem getting location data");
-
-    const dataGeo = await resGeo.json();
-    console.log(dataGeo);
-
-    // Country data
-    const res = await fetch(
-      `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
-    );
-
-    // BUG in video:
-    // if (!resGeo.ok) throw new Error('Problem getting country');
-
-    // FIX:
-    if (!res.ok) throw new Error("Problem getting country");
-
-    const data = await res.json();
-    console.log(data);
-    renderCountry(data[0]);
-  } catch (err) {
-    console.error(`${err} 💥`);
-    renderError(`💥 ${err.message}`);
-  }
-};
-whereAmI();
-whereAmI();
-whereAmI();
-console.log("FIRST");
-
-// try {
-//   let y = 1;
-//   const x = 2;
-//   y = 3;
-// } catch (err) {
-//   alert(err.message);
-// }
-)
-  .then(() => {
-    currentImg.style.display = "none";
-    return createImage("img/img-2.jpg");
-  })
-  .then((img) => {
-    currentImg = img;
-    console.log("Image 2 loaded");
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = "none";
-  })
-  .catch((err) => console.error(err));
-
-///////////////////////////////////////
-// Consuming Promises with Async/Await
-// Error Handling With try...catch
-
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-};
