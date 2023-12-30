d0a9df83de68fa4fc817ae80f608c317fb1b5f78
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-12-30 02:17:43 +0100

    await with modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -18,3 +18,22 @@ add('horse', 1);
 add('bread', 10);
 add('donkey', 2);
 console.log(cart);
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('End');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+// const lastPost = getLastPost();
+// console.log(lastPost);
+
+//NOT CLEAN BRO
+// lastPost.then(data => console.log(data));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
