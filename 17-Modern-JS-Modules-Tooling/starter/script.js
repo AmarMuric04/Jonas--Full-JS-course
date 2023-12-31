a92beb6a8cd93a0199a7b2b9ae240982fd72dbba
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2023-12-31 18:00:35 +0100

    Babel and clean code

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -104,3 +104,27 @@ console.log(stateDeepClone);
 if (module.hot) {
   module.hot.accept();
 }
+
+class Person {
+  #greeting = 'hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting} ${name}`);
+  }
+}
+
+const amar = new Person('Amar');
+
+console.log('Amar' ?? null);
+console.log(cart.find(el => el.quantity > 1));
+
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+//polifying async functions
+import 'regenerator-runtime/runtime';
+
+console.log('LOLOL');
